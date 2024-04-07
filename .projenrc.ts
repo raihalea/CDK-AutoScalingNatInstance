cd5595d286196034e2942519352da6a30fcfc20f
raiha <68338762+raihalea@users.noreply.github.com>
2024-04-07 22:04:26 +0900

    add action

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
 import { awscdk, github } from 'projen';
+import { JobPermission } from 'projen/lib/github/workflows-model';
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
@@ -18,4 +19,54 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     privateKeySecret: 'PRIVATE_KEY',
   }),
 });
+
+if (project.github) {
+  const updateSubmoduels = project.github.addWorkflow('update-submodules');
+  updateSubmoduels.on({ schedule: [{ cron: '0 0 1 * *' }], workflowDispatch: {} });
+  updateSubmoduels.addJobs({
+    updateCtfd: {
+      runsOn: ['ubuntu-latest'],
+      name: 'update-submoduels',
+      permissions: {
+        contents: JobPermission.READ,
+      },
+      steps: [
+        { uses: 'actions/checkout@v4' },
+        {
+          name: 'update submodules',
+          run: 'git submodule update --remote --recursive',
+        },
+        {
+          name: 'Find mutations',
+          id: 'crete_patch',
+          run: `|-
+          git add ./
+          git diff --staged --patch --exit-code > .repo.patch || echo "patch_created=true" >> $GITHUB_OUTPUT
+          `,
+          workingDirectory: './',
+        },
+        {
+          name: 'Generate token',
+          id: 'generate_token',
+          uses: 'actions/create-github-app-token@v1',
+          with: {
+            app_id: '${{ secrets.APP_ID }}',
+            private_key: '${{ secrets.PRIVATE_KEY }}',
+          },
+        },
+        {
+          name: 'Create Pull Request',
+          uses: 'peter-evans/create-pull-request@v6',
+          with: {
+            token: '${{ steps.generate_token.outputs.token }}',
+            message: 'chore(deps): upgrade dependencies',
+            branch: 'github-actions/upgrade',
+            title: 'chore(deps): upgrade dependencies',
+          },
+        },
+      ],
+    },
+  });
+}
+
 project.synth();
